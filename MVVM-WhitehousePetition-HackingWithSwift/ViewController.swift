d7339dc53cbda753e035c933053bc366073cf624
= <=>
2023-08-14 13:50:02 +0300

    Initial Commit

---
+++ MVVM-WhitehousePetition-HackingWithSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-WhitehousePetition-HackingWithSwift
+//
+//  Created by Mert Deniz Akbaş on 14.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
